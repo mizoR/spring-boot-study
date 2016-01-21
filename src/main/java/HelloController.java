956b8cf23b8438744401ec0ad788537497165526
mizokami <r.mizokami@gmail.com>
2016-01-22 00:42:23 +0900

    Spring MVC の簡単な使い方メモ - パスパラメータの取得

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,24 +1,18 @@
 package sample.springboot.web;
 
-import java.util.Map;
-
-import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(method=RequestMethod.GET)
+    @RequestMapping(value="/{id}/{name}", method=RequestMethod.GET)
     public void getMethod(
-            @RequestParam String id,
-            @RequestParam Map<String, String> queryParameters,
-            @RequestParam MultiValueMap<String, String> multiMap) {
-        System.out.println("id=" + id);
-        System.out.println(queryParameters);
-        System.out.println(multiMap);
+            @PathVariable int id,
+            @PathVariable String name) {
+        System.out.println("id=" + id + ", name=" + name);
     }
 }
