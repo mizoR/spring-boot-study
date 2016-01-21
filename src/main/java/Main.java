a4a3d525aeb6fa6ea17dda06d703e9b8de3a6a64
mizokami <r.mizokami@gmail.com>
2016-01-21 22:12:25 +0900

    Java コード上で Bean を定義する - 基本

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,21 @@ package sample.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         try (ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args)) {
-            Main m = ctx.getBean(Main.class);
-            m.hello();
+            Hoge h = ctx.getBean(Hoge.class);
+            System.out.println(h);
         }
     }
 
-    public void hello() {
-        System.out.println("Hello Spring Boot!!");
+    @Bean
+    public Hoge getHoge() {
+        System.out.println("Main#getHoge()");
+        return new Hoge("hoge");
     }
 }
