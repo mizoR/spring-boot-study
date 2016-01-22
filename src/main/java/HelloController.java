ef6ce749f587d7708e27f3b866d7ef4f688ae147
mizokami <r.mizokami@gmail.com>
2016-01-22 12:48:23 +0900

    Spring MVC の簡単な使い方メモ - レスポンスのステータスコードを指定する

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,16 +1,17 @@
 package sample.springboot.web;
 
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(method=RequestMethod.POST)
-    public void getMethod(@RequestBody String body) {
-        System.out.println("body=" + body);
+    @RequestMapping(method=RequestMethod.GET)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void getMethod() {
     }
 }
