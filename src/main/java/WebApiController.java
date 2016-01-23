476b1ca320ce18cd4d4ef009a7722171f1f7c50c
mizokami <r.mizokami@gmail.com>
2016-01-23 09:17:42 +0900

    例外ハンドリング - 特定の例外がスローされたときのステータスコードを指定する

---
+++ src/main/java/WebApiController.java
@@ -0,0 +1,15 @@
+package sample.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WebApiController {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public void method1() {
+        throw new MyException("test exception");
+    }
+}
